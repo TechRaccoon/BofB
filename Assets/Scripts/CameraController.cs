fb29f8641eb0bf8a9498320a5b7b9d4eb7697616
TechRaccoon <c0526035@online.camosun.ca>
2025-03-03 22:34:24 -0800

    Basic Player Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //Reference to the Player´s location
+    public Transform target;
+
+    //Offset from the Player
+    private Vector3 offset = new Vector3(0f, 2f, -8f);
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target != null) {
+
+            //Set the camera to the players position plus the offset
+            transform.position = target.position + offset;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //[SerializeField] NavMeshAgent agent;
+    [SerializeField] CharacterController charCtrl;
+    //[SerializeField] PlayerAnim anim;
+
+    //SFX
+    //[SerializeField] AudioSource playerAudio;
+    //[SerializeField] AudioClip shot;
+
+
+    //private PlayerState state;
+
+    //player variables
+    
+
+    private int bulletDamage = 4;
+
+    //singular enemy clicked by player
+    public Vector2 targetDirection;
+    private GameObject targetObject;
+
+    //movement variables
+    private float horizInput;
+    private float vertInput;
+    private float speed = 3.0f;
+    private float gravity = -9.8f;
+
+    //direction for animations 
+    private Vector2 direction;
+
+    //coroutine running to avoid multiple calls
+    private bool isDamage = false;
+    private bool isCoroutineRunning = false;
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DEBUG STATEMENT
+        //Debug.Log("PLAYER STATE: " + state.GetPlayerState());
+
+
+        //movement handle
+        horizInput = Input.GetAxis("Horizontal");
+        vertInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizInput, gravity, vertInput) * speed * Time.deltaTime;
+        charCtrl.Move(transform.TransformDirection(movement));
+
+        //Animation directions call
+        //direction = new Vector2(horizInput, vertInput).normalized;
+
+        // check if player is moving
+        //Vector2 inputVector = new Vector2(horizInput, vertInput);
+        //bool isMoving = inputVector.magnitude > 0.1f;
+
+
+        //if (state.GetPlayerState() == PlayerStates.PLAYER_DEAD)
+        //{
+        //    anim.SetDirection(direction);
+        //    GetComponent<PlayerMovement>().enabled = false;
+
+        //}
+        //else if (isDamage)
+        //{
+        //    if (!isCoroutineRunning)
+        //    {
+        //        StartCoroutine(DamageCoroutine(direction));
+        //    }
+        //}
+        //else if (isMoving) // player is moving!
+        //{
+        //    Messenger.Broadcast("PLAYER_MOVING");
+        //    anim.SetDirection(direction);
+        //}
+        //else
+        //{
+        //    if (targetObject != null && state.GetPlayerState() == PlayerStates.PLAYER_ATTACK) // Player is attacking ATTACK!!
+        //    {   //rotate player towards enemy 
+        //        anim.SetDirection(targetDirection); //target directon passed 
+        //        if (!isCoroutineRunning) // check if coroutine is running NOT animation
+        //        {
+        //            StartCoroutine(DoubleAction());
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Messenger.Broadcast("PLAYER_IDLE"); //player is NOT attacking, idle
+        //        anim.SetDirection(direction);
+        //    }
+        //}
+    }
+
+    //IEnumerator DoubleAction() //single animation 2 shots , damage dealt to enemy
+    //{
+    //    if (targetObject != null)
+    //    {
+    //        isCoroutineRunning = true;
+    //        targetObject.GetComponent<EnemyAI>().Health -= bulletDamage;
+    //        playerAudio.PlayOneShot(shot);
+    //        // Wait for the second shot.
+    //        yield return new WaitForSeconds(0.2f);
+    //    }
+    //    if (targetObject != null)
+    //    {
+    //        targetObject.GetComponent<EnemyAI>().Health -= bulletDamage;
+    //        playerAudio.PlayOneShot(shot);
+
+    //        // Wait for the next attack animation.
+    //        yield return new WaitForSeconds(0.2325f);
+    //    }
+    //    isCoroutineRunning = false;
+    //}
+
+    //private void OnHealthChanged(int oldHealth)
+    //{
+    //    //Debug.Log("ONHEALTHCHANGED CALLED");
+    //    if (health <= 0)
+    //    {
+    //        Messenger.Broadcast("PLAYER_DEAD");
+    //    }
+    //    else if (health > oldHealth)
+    //    { //Healing
+    //        Messenger.Broadcast("PLAYER_HEAL");
+    //    }
+    //    else if (health < oldHealth)
+    //    { //taking damage
+    //        isDamage = true;
+    //    }
+    //    else
+    //    {
+    //        //implement healing;
+
+    //    }
+    //}
+
+    //IEnumerator DamageCoroutine(Vector2 direction)
+    //{
+    //    isCoroutineRunning = true;
+    //    Messenger.Broadcast(GameEvent.PLAYER_DAMAGE);
+    //    anim.SetDirection(direction);
+    //    yield return new WaitForSeconds(0.15f);
+    //    isCoroutineRunning = false;
+    //    isDamage = false;
+    //}
+
+    //public void ClearTarget()
+    //{
+    //    Debug.Log("clearing target");
+    //    this.targetObject = null;
+    //}
+    //public void SetTarget(GameObject obj)
+    //{
+    //    Debug.Log("SetTarget(): " + obj.gameObject.tag);
+    //    this.targetObject = obj;
+    //}
+}
