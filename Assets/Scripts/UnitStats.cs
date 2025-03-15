5a29359e96b10951e9e057278357e38d4f37d177
TechRaccoon <c0526035@online.camosun.ca>
2025-03-15 09:50:38 -0700

    Structure

---
+++ Assets/Scripts/UnitStats.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    [SerializeField] public string unitName;
+    [SerializeField] public int unitiLevel;
+
+    [SerializeField] public int unitDamage;
+
+    [SerializeField] public int maxHP;
+    [SerializeField] public int currentHP;
+}
